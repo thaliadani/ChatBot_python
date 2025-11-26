4380cf7c98a5ebbe9de0c034082f2543b0ec2007
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2025-11-26 09:37:45 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,17 +1,31 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot = ChatBot('Chat Bot')
+bot = ChatBot(
+    'Bot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3',
+    logic_adapters = [
+        'chatterbot.logic.BestMatch',
+        'chatterbot.logic.TimeLogicAdapter']
+    )
+
+trainer = ListTrainer(bot)
 
 conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python']
 
-bot.set_trainer(ListTrainer)
 bot.train(conversa)
 
+name = input("Qual o seu nome? ")
+print("Boas vindas ao serviço de bot!")
 while True:
-    pergunta = input("Usuário: ")
-    resposta = bot.get_response(pergunta)
-    if float(resposta.confidence) > 0.5:
-        print('bot: ', resposta)
-    else:
-        print('bot: Ainda não sei responder esta pergunta')
+    try:
+        request = input(name+ ': ')
+        if request == 'Tchau' or request == 'tchau':
+            print("Bot: Tchau")
+            break
+        else:
+            response = bot.get_response(request)
+            print('Bot:', response)
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
